34bcc7f782de3de7816f3023fe05b9441afad77b
David R Connell <davidconnell12@gmail.com>
2024-02-28 14:52:42 -0600

    Add dependencies as submodules

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Igraph SE2 implementation";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nix-matlab = {
       url = "/home/voidee/clones/nix-matlab";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,34 +14,25 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = (with nix-matlab.packages.${system}; [
-          matlab
-          matlab-mlint
-          matlab-mex
-        ]) ++ (with pkgs; [
-          astyle
-          bear
-          gdb
-          gnumake
-          igraph
-
-          # igraph dependencies
-          arpack
-          bison
-          blas
-          cmake
-          flex
-          glpk
-          gmp
-          lapack
-          libxml2
-          plfit
-        ]);
-        shellHook = ''
-          export OMP_NUM_THREADS=4
-          export C_INCLUDE_PATH=${pkgs.igraph.dev}/include/igraph
-        '';
-      };
+      devShells.${system}.default =
+        (pkgs.mkShell.override { stdenv = pkgs.gcc10Stdenv; }) {
+          buildInputs = (with nix-matlab.packages.${system}; [
+            matlab
+            matlab-mlint
+            matlab-mex
+          ]) ++ (with pkgs; [
+            astyle
+            cmake
+            ninja
+            gdb
+            # igraph dependencies
+            bison
+            flex
+            libxml2
+          ]);
+          shellHook = ''
+            export OMP_NUM_THREADS=16
+          '';
+        };
     };
 }
